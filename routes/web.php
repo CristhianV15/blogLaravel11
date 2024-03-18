9f00707861733d3ac1278e86327f37261824154e
Cristhian Valladolid <cmvc99@gmail.com>
2024-03-18 10:56:09 -0500

    Enrutamiento basico desde web.php

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
+//Importa el orden de las rutas, de arriba a abajo
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::Get('/posts', function(){
+Route::get('/posts', function(){
     return "Bienvenido a los posts";
 });
+
+Route::get('/posts/{post}/{categoria?}', function($post,$categoria= null){ //mismo nombre de la variable del contenido variable
+    if($categoria){
+        return "Aqui se mostrara el contenido del post {$post} de la categoria {$categoria}";
+    }
+    return "Aqui se mostrara el contenido del post {$post}";
+});
+
+/*Route::get('/posts/{post}/{categoria}', function ($post,$categoria){
+    return "Aqui se mostrara el contenido del post {$post} de la categoria {$categoria}";
+}); Para poner dos opciones variables, pero ambos necesarios para entrar a la pagina*/
 
 //Peticiones
 /*  Get: Cuando se escribe en la url o se da click en un enlace
