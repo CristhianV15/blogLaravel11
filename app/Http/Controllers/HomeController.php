6221ac0bece8b185616a3ac14e9f4c41010e101a
Cristhian Valladolid <cmvc99@gmail.com>
2024-03-19 19:13:33 -0500

    Base simple y creación de archivos de vistas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,6 @@ class HomeController extends Controller
 {
     public function __invoke(){
       //  return view('welcome');
-      return "bienvenido a la principal";
+      return view ('home');
     }
 }
