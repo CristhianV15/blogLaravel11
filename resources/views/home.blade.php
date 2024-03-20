3fe099384ef5bf771aa42de1f4e7973038f08c0a
Cristhian Valladolid <cmvc99@gmail.com>
2024-03-19 22:24:16 -0500

    primera parte de componentes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11</title>
+
+    <script src= "https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <h1>Bienvenido a la página principal</h1>
+    <div class="max-w-4xl mx-auto px-4">
+        <h1>Bienvenido a la página principal</h1>
+    </div>
+  
+<div class="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+    <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+  </div>
+  <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+    <span class="font-medium">Danger alert!</span> Change a few things up and try submitting again.
+  </div>
+  <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+    <span class="font-medium">Success alert!</span> Change a few things up and try submitting again.
+  </div>
+  <div class="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert">
+    <span class="font-medium">Warning alert!</span> Change a few things up and try submitting again.
+  </div>
+  <div class="p-4 text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300" role="alert">
+    <span class="font-medium">Dark alert!</span> Change a few things up and try submitting again.
+  </div>
+  
+      
 </body>
 </html>
